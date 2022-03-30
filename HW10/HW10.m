2e06eab299394c61f258dd4612f79f9e42cc0d39
Jacob Foulds <jfoulds00@gmail.com>
2022-03-30 01:00:44 -0400

    add HW10 and HW11

--- HW10/HW10.m
+++ HW10/HW10.m
@@ -40,6 +40,71 @@ JAPP = m3B_sol*BG3^2 + m3C_sol*CG3^2 %lbm*in^2
 
 
 %% Problem 4
+clc,clear,close all
 
+load('conn_rod_CCW.txt')
+load('hole_B_CCW.txt')
+load('hole_C_CCW.txt')
 
+d2r = pi / 180;
+
+t = .75; %in
+rho = 0.0975437; %lbm/in^3
+
+%reverse direction of hole numbers
+hole_B_CW = zeros(size(hole_B_CCW));
+hole_B_CW(:,1) = hole_B_CCW(end:-1:1,1);
+hole_B_CW(:,2) = hole_B_CCW(end:-1:1,2);
+
+hole_C_CW = zeros(size(hole_C_CCW));
+hole_C_CW(:,1) = hole_C_CCW(end:-1:1,1);
+hole_C_CW(:,2) = hole_C_CCW(end:-1:1,2);
+
+x = [conn_rod_CCW(:,1)' hole_B_CW(:,1)' hole_C_CW(:,1)' conn_rod_CCW(1,1)];
+y = [conn_rod_CCW(:,2)' hole_B_CW(:,2)' hole_C_CW(:,2)' conn_rod_CCW(1,2)];
+
+[ geom, iner, cpmo ] = polygeom( x, y );
+
+area = geom(1);
+x_cen = geom(2);
+y_cen = geom(3);
+perimeter = geom(4);
+disp( [ ' ' ] )
+disp( [ 'Connecting Rod 3' ] )
+disp( [ ' ' ] )
+disp( [ ' area x_cen y_cen perim' ] )
+disp( [ area x_cen y_cen perimeter ] )
+I1 = cpmo(1);
+angle1 = cpmo(2);
+I2 = cpmo(3);
+angle2 = cpmo(4);
+disp( [ ' ' ] )
+disp( [ ' I1 I2' ] )
+disp( [ I1 I2 ] )
+disp( [ ' angle1 angle2' ] )
+disp( [ angle1/d2r angle2/d2r ] )
+% plot outline
+xplot = x( [ 1:end 1] );
+yplot = y( [ 1:end 1] );
+rad = 10;
+x1 = [ x_cen-rad*cos(angle1) x_cen+rad*cos(angle1) ];
+y1 = [ y_cen-rad*sin(angle1) y_cen+rad*sin(angle1) ];
+x2 = [ x_cen-rad*cos(angle2) x_cen+rad*cos(angle2) ];
+y2 = [ y_cen-rad*sin(angle2) y_cen+rad*sin(angle2) ];
+plot( xplot,yplot,'b', x_cen,y_cen,'r+', ...
+ x1,y1,'g:', x2,y2,'g:' )
+axis( [ 0 rad 0 rad ] )
+axis square
+xlim([-2 6])
+ylim([-4 4])
+
+m3 = geom(1)*t*rho
+
+%BG3 from plot
+BG3 = x_cen
+
+%Calculate controidal polar mass mmoent of intertia from centroidal polar
+%area moment of inertia
+
+J_boundary = t*rho*cpmo(5)
 
