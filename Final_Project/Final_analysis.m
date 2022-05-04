af68661c0cfd1683ce98fc0a4c156811728abc6f
Jacob Foulds <jfoulds00@gmail.com>
2022-05-04 15:28:11 -0400

    add WM data

--- Final_Project/Final_analysis.m
+++ Final_Project/Final_analysis.m
@@ -1,2 +1,12 @@
 clc,clear,close all
 
+% data = load('Final_model_WM.dta');
+% 
+% t_WM = data(:,1);
+% P_WM = data(:,[2 6]);
+% V_WM = data(:,[3 7]);
+% A_WM = data(:,[4 8]);
+% 
+% save('data_WM','t_WM','P_WM','V_WM','A_WM')
+
+load('data_WM.mat')
